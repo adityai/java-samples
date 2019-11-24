1d40ce74253ecd0f6d510e94c4c6d303f8bc7184
adityaii <adityaii@gmail.com>
2019-11-24 13:20:21 -0500

    Added comments

--- src/main/java/com/iaditya/RunnableSample.java
+++ src/main/java/com/iaditya/RunnableSample.java
@@ -4,6 +4,8 @@ public class RunnableSample {
 
   public static void main(String... args) {
     System.out.println("Test");
+    
+    //Pre Java 8
       Runnable runnable = new Runnable(){
            @Override
            public void run(){
@@ -12,6 +14,7 @@ public class RunnableSample {
          };
     runnable.run();
     
+    //Java 8 through 13
     Runnable runnableLambda = () -> System.out.println("Hello world lambda!");
     runnableLambda.run();
 
