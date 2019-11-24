cc9b97d3e786f6fc18b89813fc76d00d1fe5416a
adityaii <adityaii@gmail.com>
2019-11-24 17:17:21 -0500

    Add Local variable var

---
+++ src/main/java/com/iaditya/LocalVariableVar.java
@@ -0,0 +1,10 @@
+package com.iaditya;
+
+public class LocalVariableVar {
+  
+  public static void main(String... args) {
+    System.out.println("Local variable var");
+    var test = "test var";
+    System.out.println(test);
+  }
+}
